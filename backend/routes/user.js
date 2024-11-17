a037a2e802d9b379bad3fecc6ccf2f1259340426
Sarvesh0955 <sarvesh0955@gmail.com>
2024-11-17 15:57:16 +0530

    added isAdmin feild

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -24,7 +24,7 @@ router.post('/login', async (req, res) => {
         req.session.address= user.address;
         req.session.isLogin = true;
         req.session.isAdmin = user.isAdmin;
-        res.json({ message: 'Login successful', user: { _id:user._id,name: user.name, email: user.email , phoneNumber: user.phoneNumber, address : user.address} });
+        res.json({ message: 'Login successful', user: { _id:user._id,name: user.name, email: user.email , phoneNumber: user.phoneNumber, address : user.address, isAdmin : user.isAdmin} });
     } catch (e) {
         console.log('Error in login:', e);
         res.status(500).json({ message: 'Error logging in.' });
