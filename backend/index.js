82c252eece112264635b6f0afb738c78db3e0414
Sarvesh0955 <sarvesh0955@gmail.com>
2025-05-23 13:23:10 +0530

    modified backend to work on deployment

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,13 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 } 
+    cookie: { 
+        secure: false, 
+        httpOnly: true, 
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite: 'none',
+        secure: true
+    } 
 }));
 
 // MongoDB connection
